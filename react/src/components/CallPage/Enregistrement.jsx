eeaf0dcd24a3c115967b1f7a3ef09bc172bfd9da
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2025-06-26 16:26:55 +0200

    Listen audio record

--- react/src/components/CallPage/Enregistrement.jsx
+++ react/src/components/CallPage/Enregistrement.jsx
@@ -1,13 +1,19 @@
 export default function Enregistrement({ callDetails }) {
 
-    if (!callDetails) {
-        return <p className="text-gray-600 italic">Aucun enregistrement d'appel disponible.</p>;
+    if (!callDetails && !callDetails.audioPath) {
+        return <div className={"box fc g0-5"}>
+            <h2 className="text-2xl font-bold">Enregistrement</h2>
+            <p className="text-gray-600 italic">Aucun enregistrement d'appel disponible.</p>
+        </div>
     }
 
     return (
         <div className={"box fc g0-5"}>
             <h2 className="text-2xl font-bold">Enregistrement</h2>
-            <p>{callDetails.audioPath ? callDetails.audioPath : 'Non enregistré'}</p>
+            <audio controls className="w-full">
+                <source src={`${import.meta.env.VITE_API_URL}/${callDetails.audioPath}`} type="audio/mpeg"/>
+                Votre navigateur ne supporte pas la balise audio.
+            </audio>
         </div>
     );
 }
